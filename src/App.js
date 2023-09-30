97b2a1e8a234e9d6026104983f4c2991a95e6b2f
MarieWalyFall <waliettefall@gmail.com>
2023-09-30 14:46:53 +0000

    update commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
-import TablePage from './pages/TablePage';
-import AddMedicationPage from './pages/AddMedicationPage'
-import AddTicketPage from './pages/AddTicketPage'
-import LoginPage from './pages/LoginPage';
-
+import History from './pages/History';
 function App() {
   return (
-    <div>
-      <LoginPage/>
-
-    </div>
-  );
+    <History whichPage={'pharmacie'} allOpen={true} />
+  )
 }
 
 export default App;
